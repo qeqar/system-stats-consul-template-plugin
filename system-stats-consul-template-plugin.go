9deb8188a7074b12a8bf71aef6c73825c176745e
Mark Eisenblaetter <mark.eisenblaetter@kaufhof.de>
2019-01-30 10:49:09 +0100

    add new statsType cpuCount

--- system-stats-consul-template-plugin.go
+++ system-stats-consul-template-plugin.go
@@ -17,12 +17,19 @@ func cpuInfo() {
 	fmt.Println(cpuStats)
 }
 
+func cpuCount() {
+	count, _ := cpu.Counts(true)
+	fmt.Println(count)
+}
+
 func main() {
 	for _, statsType := range os.Args {
 		if statsType == "memory" {
 			memory()
 		} else if statsType == "cpuInfo" {
 			cpuInfo()
+		} else if statsType == "cpuCount" {
+			cpuCount()
 		}
 	}
 	os.Exit(0)
